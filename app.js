2dbe3fc15a3ff75b9c9c7acfae287459cf1fcdc9
Your Name <you@example.com>
2017-01-10 14:57:03 +0300

    ic

--- app.js
+++ app.js
@@ -16,8 +16,14 @@ options = {
   path: '/oauth2/v4/token',
   method: 'POST',
   headers: {
-      'Content-Type': 'application/json',
-  }
+      'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  
+  form: {'code':'4/P7q7W91a-oMsCeLvIaQm6bTrgtp7',
+		'client_id':'920263213693-i234smkj1crhoquepvdmshin9k8qoptc.apps.googleusercontent.com',
+		'client_secret':'eRoYBXEU6TlG1xc9EWtuaf9y',
+		redirect_uri='https://peaceful-waters-40664.herokuapp.com/callback'&
+		grant_type=authorization_code}
 };
 
 
