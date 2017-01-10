971f166b93f6c8d2cb668ae42d549a41c57b2ee0
Your Name <you@example.com>
2017-01-10 18:47:01 +0300

    ic

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/callback', function (req, res) {
 
   console.log('Google did sent a response');
   var qsFromReq = querystring.parse(urlparse(req.url));
-
+  console.log(qsFromreq);
   var data = querystring.stringify({code:qsFromReq.code,
 		client_id:'920263213693-i234smkj1crhoquepvdmshin9k8qoptc.apps.googleusercontent.com',
 		client_secret:'eRoYBXEU6TlG1xc9EWtuaf9y',
