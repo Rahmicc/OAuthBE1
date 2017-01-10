e7a04c01688617474dd12458f3cfa94a8ea57395
Your Name <you@example.com>
2017-01-10 13:41:47 +0300

    ic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express"),
 app = express(),
 port = process.env.PORT || 1881,
 http = require("http"),
+
 options = {
   hostname: 'https://www.googleapis.com',
   port: 443,
@@ -18,19 +19,11 @@ app.get('/callback', function (req, res) {
   console.log('Google did sent a response');
   console.log (req);
   console.log('sending POST request...');
-  var req = http.request(options, function(res) {
+  var postReq = http.request(options, function(res) {
   	console.log('Status: ' + res.statusCode);
   });
-});
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
-// write data to request body
-req.write('{"string": "Hello, World"}');
-req.end();
-  
   res.send('Hello World!')
-})
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
